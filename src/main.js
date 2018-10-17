6f73968d53a5a6b2c4da1ab2441edbdb9b1d3f83
uday-r <udayreddymunna@gmail.com>
2018-10-17 08:06:25 +0530

    Using mongoose to connect to DB. Using docker's env variable DB_URI to connect to DB.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routeHandler = require('./routeHandler');
 const handleListen = require('./handleListen');
+
+const DB_URI = process.env.DB_URI;
+mongoose.connect(DB_URI);
+const db = mongoose.connection;
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 app.get('/', routeHandler.hello);
 /* eslint-disable no-console */
-app.listen(PORT, handleListen(console.log, PORT));
+
 module.exports = app;
+
+db.on(
+  'error',
+  (err) => {
+    console.error(
+      'Cannot connect to Mongodb server\nError message is:',
+      err.message,
+    );
+    process.exit(1);
+  },
+);
+
+db.on(
+  'open',
+  () => {
+    console.info('Server starting...');
+    console.info('Connected to the database backend.');
+    app.listen(PORT, handleListen(console.log, PORT));
+  },
+);
